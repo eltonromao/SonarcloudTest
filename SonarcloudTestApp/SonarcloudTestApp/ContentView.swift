61683e01c72ffff5e78ffa1980d6539e178bd171
Elton Romao <elton.romao@care.com>
2023-11-21 13:45:56 -0300

    first commit

---
+++ SonarcloudTestApp/SonarcloudTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SonarcloudTestApp
+//
+//  Created by Elton Jhony Romão de Oliveira on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SonarcloudTestApp/SonarcloudTestApp/SonarcloudTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarcloudTestAppApp.swift
+//  SonarcloudTestApp
+//
+//  Created by Elton Jhony Romão de Oliveira on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SonarcloudTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
